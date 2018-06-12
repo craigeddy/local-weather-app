5a047695a006bdc91da1f404be2bb7e5f6e40b2b
Craig <craig.eddy@gmail.com>
2018-06-12 18:05:35 -0400

    Starting on Flex Layout implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,25 @@ import { Component } from '@angular/core'
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  template: `
+  <div>
+  <mat-toolbar color="primary">
+    <span>LocalCast Weather</span>
+  </mat-toolbar>
+  <div fxLayoutAlign="center">
+    <div class="mat-caption"> Your city, your forecast, right now!</div></div>
+  <div fxLayout="row">
+    <div fxFlex></div>
+    <mat-card fxFlex="300px">
+      <mat-card-header class="mat-typography">
+        <mat-card-title><div class="mat-title">Current Weather</div></mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        <app-current-weather></app-current-weather>
+      </mat-card-content>
+    </mat-card>
+  <div fxFlex></div>
+</div>
+`,
 })
 export class AppComponent {}
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,15 @@
-import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material'
+import {
+  MatButtonModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+} from '@angular/material'
 
 import { NgModule } from '@angular/core'
 
 @NgModule({
-  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule, MatCardModule],
   declarations: [],
 })
 export class MaterialModule {}
