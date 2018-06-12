db3bf49abbf7b7524050ccbcdf2bc5eecc457d06
Craig <craig.eddy@gmail.com>
2018-06-12 17:41:00 -0400

    Added Angular Material, HammerJS, and Angular Flex Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,20 @@ import { CurrentWeatherComponent } from './current-weather/current-weather.compo
 import { WeatherService } from './weather/weather.service'
 
 import { HttpClientModule } from '@angular/common/http'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MaterialModule } from './material.module'
+import 'hammerjs'
+import { FlexLayoutModule } from '@angular/flex-layout'
 
 @NgModule({
   declarations: [AppComponent, CurrentWeatherComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    FlexLayoutModule,
+  ],
   providers: [WeatherService],
   bootstrap: [AppComponent],
 })
---
+++ src/app/material.module.ts
@@ -0,0 +1,10 @@
+import { MatButtonModule, MatToolbarModule, MatIconModule } from '@angular/material'
+
+import { NgModule } from '@angular/core'
+
+@NgModule({
+  imports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  exports: [MatButtonModule, MatToolbarModule, MatIconModule],
+  declarations: [],
+})
+export class MaterialModule {}
