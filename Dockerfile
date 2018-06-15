d727ac765a90f5dadddd4e653467dce5956a2498
Craig Eddy <craig.eddy@gmail.com>
2018-06-14 22:19:35 -0400

    Docker deployment

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+FROM duluca/minimal-node-web-server:8.11.1
+WORKDIR /usr/src/app
+COPY dist public
+EXPOSE 3000
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
   production: true,
+  appId: '5bad71192df8c656b662a0d6c4995953',
+  baseUrl: 'https://',
 }
